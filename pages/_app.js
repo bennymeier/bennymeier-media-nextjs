5f830514c686b4c9142a7776cbee31f613329b08
Benny Meier <meier.benjamin@web.de>
2020-11-02 15:44:57 +0100

    Add header menu, prettify all

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core';
 
 function MyApp({ Component, pageProps }) {
-  return (<ThemeProvider theme={theme}>
-    <CSSReset />
-    <Component {...pageProps} />
-  </ThemeProvider>)
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { Heading } from '@chakra-ui/core';
+import Header from '../components/header';
 
 export default function Home() {
   return (
@@ -7,12 +9,12 @@ export default function Home() {
         <title>Benny Meier Media // Fullstack Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
       <main>
+        <Heading>Benny Meier</Heading>
       </main>
 
-      <footer>
-      </footer>
+      <footer></footer>
     </div>
-  )
+  );
 }
