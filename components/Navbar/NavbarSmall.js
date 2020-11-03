a569bfaa1018a1fb696380511ef14f65d957b146
Benny Meier <meier.benjamin@web.de>
2020-11-03 17:41:45 +0100

    Add white logo, add border bottom to navbar

--- components/Navbar/NavbarSmall.js
+++ components/Navbar/NavbarSmall.js
@@ -1,18 +1,33 @@
 import styles from './NavbarSmall.module.css';
 import Image from 'next/image';
 import LayoutToggle from '../LayoutToggle/LayoutToggle';
+import { Box, useColorModeValue } from '@chakra-ui/core';
+import { useEffect } from 'react';
+
+const Logo = () => {
+  const image = useColorModeValue('black', 'white');
+  return <Image className="logo" src={`/img/logo_${image}.png`} unsized />;
+};
 
 const NavbarSmall = () => {
+  const bg = useColorModeValue('white', 'gray.800');
+  const borderColor = useColorModeValue('gray.200', '#ffffff29');
+
   return (
     <>
-      <nav className={styles['sticky-navbar']}>
+      <Box
+        className={styles['sticky-navbar']}
+        as="nav"
+        bg={bg}
+        borderBottom={`1px solid ${borderColor}`}
+      >
         <div>
-          <Image className="logo" src="/img/logo_black.png" unsized />
+          <Logo />
         </div>
         <div>
           <LayoutToggle />
         </div>
-      </nav>
+      </Box>
     </>
   );
 };
