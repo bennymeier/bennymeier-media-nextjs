350a94de8abc7a80c3c8a76376322936e6b25061
Benny Meier <meier.benjamin@web.de>
2020-11-04 15:20:58 +0100

    Add credit card component page

--- pages/projects/react-credit-card-component.js
+++ pages/projects/react-credit-card-component.js
@@ -5,40 +5,8 @@ const Description = () => {
   return (
     <Box mb="2em">
       <Text>
-        Since there are almost only YouTube downloaders with adblockers on the
-        internet, I developed one myself. The design comes with a light and dark
-        mode. In the backend several APIs are provided for the frontend.
-      </Text>
-      <br />
-      <Text>There are the following:</Text>
-      <UnorderedList>
-        <ListItem>
-          /suggestions
-          <Text>
-            If the user does not enter a link, this API is called and the user
-            gets suggestions.
-          </Text>
-        </ListItem>
-        <ListItem>
-          /metainfo
-          <Text>
-            This is where the basic information of the video is retrieved, such
-            as title, description, views, likes and dislikes.
-          </Text>
-        </ListItem>
-        <ListItem>
-          /watch
-          <Text>
-            With the two parameters Format and URL the download is prepared
-            here. The title comes from the metainfo API and is used as filename.
-            But you have to delete Special Characters first.
-          </Text>
-        </ListItem>
-      </UnorderedList>
-      <br />
-      <Text>
-        Then the appropriate Content-Disposition Header is set, so that the file
-        is downloadable.
+        Out of boredom I once developed a credit card component with React and
+        styled it with Sass.
       </Text>
     </Box>
   );
@@ -47,12 +15,12 @@ const Description = () => {
 const Project = () => {
   return (
     <Page
-      stack={['React', 'Node.js', 'Axios']}
+      stack={['React', 'Sass']}
       type="Personal"
-      images={['projects/youtube-downloader']}
+      images={['projects/react-credit-card-component']}
       description={Description}
-      title="YouTube Downloader"
-      url="https://youtubdle.com"
+      title="React Credit Card Component"
+      url="http://react-credit-card-component.vercel.app/"
     />
   );
 };
