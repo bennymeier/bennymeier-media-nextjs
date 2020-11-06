2e8620d5851e896851d4cc00890adf264efdf886
Benny Meier <meier.benjamin@web.de>
2020-11-06 18:32:16 +0100

    Add SEO tags

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,40 @@ import Contact from '../components/Contact/Contact';
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>Benny Meier Media // Frontend Developer</title>
+      <Head lang="en">
+        <title>Benny Meier Media // Fullstack Developer</title>
+        <meta
+          name="description"
+          content="Benny Meier is a fullstack developer from Freiburg, Germany. He loves web design and web development. He is an enthusiast of React, TypeScript, Node.js and MongoDB."
+        />
+
+        <meta property="og:image" content="" />
+        <meta
+          property="og:title"
+          content="Benny Meier Media // Fullstack Developer"
+        />
+        <meta
+          property="og:description"
+          content="Benny Meier is a fullstack developer from Freiburg, Germany. He loves web design and web development. He is an enthusiast of React, TypeScript, Node.js and MongoDB."
+        />
+        <meta property="og:url" content="https://bennymeier-media.de" />
+        <meta
+          property="og:site_name"
+          content="Benny Meier Media // Fullstack Developer"
+        />
+
+        <meta name="twitter:card" content="" />
+        <meta name="twitter:url" content="https://bennymeier-media.de" />
+        <meta
+          name="twitter:title"
+          content="Benny Meier Media // Fullstack Developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Benny Meier is a fullstack developer from Freiburg, Germany. He loves web design and web development. He is an enthusiast of React, TypeScript, Node.js and MongoDB."
+        />
+        <meta name="twitter:image" content="" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
