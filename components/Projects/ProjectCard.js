0ea882d7b92ca20efdddf2bb48911dcd000068e3
Benny <meier.benjamin@web.de>
2021-07-26 15:52:22 +0200

    Update packages

--- components/Projects/ProjectCard.js
+++ components/Projects/ProjectCard.js
@@ -1,10 +1,10 @@
-import { Box, Heading, Link, Text, useColorModeValue } from '@chakra-ui/react';
-import { ChevronRightIcon } from '@chakra-ui/icons';
-import Image from 'next/image';
-import NextLink from 'next/link';
+import { Box, Heading, Link, Text, useColorModeValue } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import Image from "next/image";
+import NextLink from "next/link";
 
 const ProjectCard = ({ title, description, images, url, internalUrl }) => {
-  const linkColor = useColorModeValue('gray.800', 'teal.300');
+  const linkColor = useColorModeValue("gray.800", "teal.300");
 
   return (
     <Box maxWidth="600px">
@@ -12,13 +12,15 @@ const ProjectCard = ({ title, description, images, url, internalUrl }) => {
         return (
           <NextLink href={`/projects/${internalUrl}`} passHref key={image}>
             <Link aria-label={`Image preview of ${title}`}>
-              <Image
-                alt={`Image preview of ${title}`}
-                key={image}
-                src={`/img/projects/${image}.png`}
-                layout="fill"
-                className="zoom-in round"
-              />
+              <Box className="img-container">
+                <Image
+                  alt={`Image preview of ${title}`}
+                  key={image}
+                  src={`/img/projects/${image}.png`}
+                  layout="fill"
+                  className="zoom-in round"
+                />
+              </Box>
             </Link>
           </NextLink>
         );
