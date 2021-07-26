e87ae5a3a94110cb0bf540db7f7bf9e9005e4766
Benny <meier.benjamin@web.de>
2021-07-26 15:54:07 +0200

    Add img-container to reference images

--- components/Mockup/Mockup.js
+++ components/Mockup/Mockup.js
@@ -1,11 +1,11 @@
-import styles from './Mockup.module.css';
-import Image from 'next/image';
-import { Box } from '@chakra-ui/react';
+import styles from "./Mockup.module.css";
+import Image from "next/image";
+import { Box } from "@chakra-ui/react";
 
 const Mockup = ({ imageUrl }) => {
   return (
     <Box
-      className={`${styles['browser-mockup']} ${styles['with-url']} zoom-in`}
+      className={`${styles["browser-mockup"]} ${styles["with-url"]} zoom-in img-container`}
       mb="1em"
     >
       <Image
